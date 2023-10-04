794864cad1ed0675aa0871293d877a29a0ccff30
yousef <76182050+josephsabet@users.noreply.github.com>
2023-10-05 00:06:42 +0300

    first-com

---
+++ Javascript-Question/q2.js
@@ -0,0 +1,23 @@
+
+function Sum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
+// anthor sol
+function Sum(nums, target) {
+    const numMap = {}; 
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (complement in numMap) {
+            return [numMap[complement], i];
+        }
+        numMap[nums[i]] = i;
+    }
+    return [];
+}
