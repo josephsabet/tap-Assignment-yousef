f4c24f904cb5a92e9a9ae32064768da2f8f7f22d
yousef <76182050+josephsabet@users.noreply.github.com>
2023-10-09 09:12:48 +0300

    ss

--- Javascript-Question/q2.js
+++ Javascript-Question/q2.js
@@ -1,5 +1,5 @@
 
-function Sum(nums, target) {
+function toSum(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
@@ -10,7 +10,7 @@ function Sum(nums, target) {
     return [];
 }
 // anthor sol
-function Sum(nums, target) {
+function toSum(nums, target) {
     const numMap = {}; 
     for (let i = 0; i < nums.length; i++) {
         const complement = target - nums[i];
